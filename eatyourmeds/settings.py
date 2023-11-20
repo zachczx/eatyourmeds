c46b46a349e8697990d710a20d502648c59160c2
zachczx <zixian.chen@gmail.com>
2023-11-20 22:38:37 +0800

    Update settings.py

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['146.190.97.144']
 
 
 # Application definition
