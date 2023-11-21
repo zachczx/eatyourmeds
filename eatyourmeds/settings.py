8d2ad23824746dca2e50e8d92a9a0746db840296
zachczx <zixian.chen@gmail.com>
2023-11-21 09:45:04 +0800

    Host

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['146.190.97.144']
+ALLOWED_HOSTS = ['146.190.97.144', 'localhost', '127.0.0.1']
 
 # Application definition
 
