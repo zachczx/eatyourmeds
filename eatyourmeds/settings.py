04c8cf650f294cce15e4c54506e99b2acfb15eeb
zachczx <zixian.chen@gmail.com>
2023-11-21 10:00:46 +0800

    Debug val

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -13,7 +13,10 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
