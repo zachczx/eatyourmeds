c305eed1096fa6a24278285bd82e06e8eeb5f467
zachczx <zixian.chen@gmail.com>
2023-11-21 11:32:05 +0800

    env

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -9,18 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from pathlib import Path
 import environ
 import os
-
-env = environ.Env(
-    ALLOWED_HOSTS=(list, ['zixianchen.com','146.190.97.144'])
-)
-environ.Env.read_env()
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, 'eatyourmeds/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -29,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=[str])
 
