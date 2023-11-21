d02f86739ecb841c37a7892b1bea78ad5b5a5e0d
zachczx <zixian.chen@gmail.com>
2023-11-21 23:18:27 +0800

    Fix error

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'eatyourmeds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates/trackerapp')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
