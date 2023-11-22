e8db119636df46456ce3d2cdb5cf2555b6637dd3
zachczx <zixian.chen@gmail.com>
2023-11-22 13:27:54 +0800

    shifted static files

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR #single root directory from where the Django application will serve the static files in production.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
