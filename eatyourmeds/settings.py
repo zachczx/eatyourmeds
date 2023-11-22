cf7288309e472b9a2a9b077fe1bee66d19433433
zachczx <zixian.chen@gmail.com>
2023-11-22 13:35:19 +0800

    Update settings.py

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "aboutme/static"),
 ]
-STATIC_ROOT = BASE_DIR #single root directory from where the Django application will serve the static files in production.
+STATIC_ROOT = os.path.join(BASE_DIR, "assets") #single root directory from where the Django application will serve the static files in production.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
