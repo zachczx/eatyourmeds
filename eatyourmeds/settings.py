4f8196deda5903baa3091ff193d71e269cde0a12
zachczx <zixian.chen@gmail.com>
2023-11-22 12:29:13 +0800

    static

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #single root directory from where the Django application will serve the static files in production.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
