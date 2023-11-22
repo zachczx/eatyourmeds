ccf514230a5184e23013972e8b1c2a376d02c7b2
zachczx <zixian.chen@gmail.com>
2023-11-22 13:06:09 +0800

    Update settings.py

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "static/aboutme"),
     os.path.join(BASE_DIR, "static/trackerapp"),
 ]
