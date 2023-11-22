60ca401f21bc585c6c34d36f444b6840c07904f5
zachczx <zixian.chen@gmail.com>
2023-11-22 14:21:48 +0800

    Update settings.py

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -125,15 +125,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATICFILES_FINDER = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticassets"),
 ]
