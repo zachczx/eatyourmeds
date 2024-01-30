1886c6444eaee496baa4b06db9e2f1e72c9e7d44
zachczx <zixian.chen@gmail.com>
2024-01-30 22:12:00 +0800

    Update views.py

--- aboutme/views.py
+++ aboutme/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 
 # Create your views here.
 
-@cache_page(60 * 150000000000)
+@cache_page(60 * 15)
 def home(request):
     return render(request, 'aboutme/home.html')
 
