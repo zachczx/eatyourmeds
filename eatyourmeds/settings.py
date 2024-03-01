97d7ccf310e12dd60fb44707b25a6dcb5a353471
zachczx <zixian.chen@gmail.com>
2024-03-01 10:57:37 +0800

    -

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'eatyourmeds/.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'True'
+DEBUG = env('DEBUG') == 'False'
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=[str])
 
