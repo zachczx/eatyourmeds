db85928b4b17c22f8be085697684c6b7b91bb262
zachczx <zixian.chen@gmail.com>
2024-07-06 11:36:45 +0800

    Changed .env location

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent #gets to folder with manage.py
 
 # Take environment variables from .env file
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, 'eatyourmeds/.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
