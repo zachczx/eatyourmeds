41e2f26a973e002a4f9ad0a75fe084285a774290
zachczx <zixian.chen@gmail.com>
2024-07-06 10:32:20 +0800

    Update settings.py

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -170,7 +170,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
 # STATIC_ROOT = env('STATIC_ROOT')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 ######## for sass bootstrap ######## 
 STATICFILES_FINDERS = (
