c5f0d0cc9502f767a311a13fc3045cece255dbfe
zachczx <zixian.chen@gmail.com>
2024-07-06 16:04:08 +0800

    -

--- eatyourmeds/settings.py
+++ eatyourmeds/settings.py
@@ -173,7 +173,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = '/app/staticfiles'
 
 ######## for sass bootstrap ######## 
 STATICFILES_FINDERS = (
